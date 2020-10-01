29177c729066a043baab5af5d96f49c16614966c
GolotaRoman <roman.golota11@gmail.com>
2020-10-01 13:19:41 +0300

    Исправлено согласно замечаниям

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@ const NAMES =
   `Екатерина`
 ];
 
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 1;
+const MAX_COMMENTS = 5;
 const PICTURES_QUANTITY = 25;
 const similarElement = document.querySelector(`.pictures`);
 const randomUsersTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
@@ -29,30 +33,40 @@ const randomInteger = (min, max) => {
   return Math.floor(rand);
 };
 
+const commentsGenerator = () => {
+  const comments = [];
+    for (let i = 0; i < randomInteger(MIN_COMMENTS, MAX_COMMENTS); i++) {
+
+    const comment = {
+        avatar: `img/avatar` + randomInteger(0, 6) + `.svg`,
+        message: COMMENT_MESSAGES[randomInteger(0, COMMENT_MESSAGES.length - 1)],
+        name: NAMES[randomInteger(0, NAMES.length - 1)]
+    }
+    comments.push(comment)
+  }
+  return comments;
+}
+
 const randomUser = (i = 0) => {
-  let user = {
+  const user = {
     url: `photos/` + i + `.jpg`,
     description: `Some description`,
-    likes: randomInteger(15, 200),
-    comments: {
-      avatar: `img/avatar-6.svg`,
-      message: COMMENT_MESSAGES[randomInteger(0, COMMENT_MESSAGES.length)],
-      name: NAMES[randomInteger(0, NAMES.length - 1)]
-    }
+    likes: randomInteger(MIN_LIKES, MAX_LIKES),
+    comments: commentsGenerator()
   };
   return user;
 };
 
-let fragment = document.createDocumentFragment();
+const fragment = document.createDocumentFragment();
 for (let i = 1; i <= PICTURES_QUANTITY; i++) {
 
 
-  let generatedUser = randomUser(i);
+  const generatedUser = randomUser(i);
   randomUsersTemplate.querySelector(`.picture__img`).src = generatedUser.url;
-  randomUsersTemplate.querySelector(`.picture__comments`).textContent = generatedUser.comments.message;
+  randomUsersTemplate.querySelector(`.picture__comments`).textContent = generatedUser.comments.length;
   randomUsersTemplate.querySelector(`.picture__likes`).textContent = generatedUser.likes;
 
-  let userELement = randomUsersTemplate.cloneNode(true);
+  const userELement = randomUsersTemplate.cloneNode(true);
 
   fragment.appendChild(userELement);
 }
