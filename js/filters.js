cead45ae60036914295d153539ff44e80975b6df
GolotaRoman <roman.golota11@gmail.com>
2020-11-02 17:22:05 +0200

    Исправлено согласно последним замечаниям

--- js/filters.js
+++ js/filters.js
@@ -1,19 +1,48 @@
 'use strict';
 
 (function () {
+
+  let DEBOUNCE_INTERVAL = 500;
+
+  window.debounce = function (cb) {
+    let lastTimeout = null;
+
+    return function (...parameters) {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb(...parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
   window.filter = `all`;
   const randomFilter = document.querySelector(`#filter-random`);
   const discussed = document.querySelector(`#filter-discussed`);
   const defaultFilter = document.querySelector(`#filter-default`);
 
-  const removePictures = () => {
+  const renderPictures = (users) => {
+    if (window.filter === `all`) {
+      window.util.render(users);
+    } else if (window.filter === `random`) {
+      const randomUsers = users.sort(() => Math.random() - Math.random()).slice(0, 10);
+
+      window.util.render(randomUsers);
+    } else if (window.filter === `discussed`) {
+      const discussedUsers = users.sort((a, b) => b.comments.length - a.comments.length);
+      window.util.render(discussedUsers);
+    }
+  };
+
+  const removePictures = window.debounce(() => {
     const length = document.querySelectorAll(`.picture`).length;
     for (let i = 0; i < length; i++) {
       document.querySelector(`.picture`).remove();
     }
-    window.load();
+    renderPictures(window.load.resultFromServer);
     document.querySelector(`.img-filters__button--active`).classList.remove(`img-filters__button--active`);
-  };
+  });
 
   randomFilter.addEventListener(`click`, function () {
     window.filter = `random`;
--- js/load.js
+++ js/load.js
@@ -2,28 +2,13 @@
 
 (function () {
 
-  let DEBOUNCE_INTERVAL = 500; // ms
-
-  window.debounce = function (cb) {
-    let lastTimeout = null;
-
-    return function (...parameters) {
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(function () {
-        cb(...parameters);
-      }, DEBOUNCE_INTERVAL);
-    };
-  };
-
   const URL = `https://21.javascript.pages.academy/kekstagram/data`;
   const TIMEOUT = 10000;
   const STATUS_OK = 200;
   const filters = document.querySelector(`.img-filters`);
 
+  window.load = () => {
 
-  window.load = window.debounce(() => {
     const onError = (errorMessage) => {
       const node = document.createElement(`div`);
       node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
@@ -40,13 +25,6 @@
 
       if (window.filter === `all`) {
         window.util.render(users);
-      } else if (window.filter === `random`) {
-        const randomUsers = users.sort(() => Math.random() - Math.random()).slice(0, 10);
-
-        window.util.render(randomUsers);
-      } else if (window.filter === `discussed`) {
-        const discussedUsers = users.sort((a, b) => b.comments.length - a.comments.length);
-        window.util.render(discussedUsers);
       }
 
       filters.classList.remove(`img-filters--inactive`);
@@ -59,6 +37,11 @@
     xhr.addEventListener(`load`, function () {
       if (xhr.status === STATUS_OK) {
         onSuccess(xhr.response);
+        const resultFromServer = xhr.response;
+
+        window.load = {
+          resultFromServer
+        };
       } else {
         onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
       }
@@ -74,8 +57,8 @@
 
     xhr.open(`GET`, URL);
     xhr.send();
-  });
 
+  };
 
   window.load();
 
